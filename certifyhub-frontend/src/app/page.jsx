0c37fbc451484c2a96bd506a8226f553003301fa
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2025-03-21 23:14:43 +0530

    Updated packages to the latest versions

---
+++ certifyhub-frontend/src/app/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">CertifyHub</h1>
+        <p>
+          A platform for developers to build and issue verifiable credentials
+        </p>
+      </main>
+    </div>
+  );
+}
